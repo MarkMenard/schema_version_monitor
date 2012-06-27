248e02d58ae346bf04d78affa0d1c059932c4d70
Jim C <pairing3@enablelabs.com>
2012-06-27 11:19:05 -0400

    Adds test for ActiveRecord establish_connection Refactor

--- lib/schema_migration_monitor/monitor.rb
+++ lib/schema_migration_monitor/monitor.rb
@@ -9,10 +9,13 @@ module ActiveRecord
 
       def establish_connection(name, spec)
         result = establish_connection_without_monitor(name,spec)
-        SchemaMigrationMonitor::Monitor.new.execute
+        monitor_migrations
         result
       end
 
+      def monitor_migrations
+        SchemaMigrationMonitor::Monitor.new.execute
+      end
     end
   end
 end
@@ -28,32 +31,44 @@ module SchemaMigrationMonitor
     def execute
       @pending_migrations = get_pending_migrations
       return if @pending_migrations.empty?
-      prompt_user
+      output_report
     end
 
-    def prompt_user
-      text_lines = prompt_user_text.split("\n")
-
-      longest_line = text_lines.inject(0) do |memo, arg|
-        ll = arg.to_s.length
-        memo = ll if ll > memo
-        memo
-      end
-
-      @output_stream.write("\e[1;31m\n")
-      @output_stream.write("*" * (longest_line + 6) + "\n")
-      text_lines.each do |line|
-        @output_stream.write "** #{line}#{ " " * (longest_line - line.to_s.length) } **\n"
-      end
-      @output_stream.write("*" * (longest_line + 6) + "\n")
-      @output_stream.write("\n\e[0m")      
+    def output_report
+      write_boxed_output(report_text.split("\n"))
     end
 
-    def prompt_user_text
+    def report_text
       res = "The following migration(s) need to be run:"
       res << @pending_migrations.map { |migration| "\n  - #{migration.filename}" }.join
-      #res << "\nWould you like to run these migrations now? [Y/N]"
-      res
+    end
+
+    def write_boxed_output(text_lines)
+      longest_line = get_longest_line(text_lines)
+
+      wrap_output_in_red do
+        @output_stream.write("*" * (longest_line + 6) + "\n")
+
+        text_lines.each do |line|
+          @output_stream.write "** #{line}#{ " " * (longest_line - line.to_s.length) } **\n"
+        end
+
+        @output_stream.write("*" * (longest_line + 6) + "\n")
+      end
+    end
+
+    def wrap_output_in_red
+      @output_stream.write("\e[1;31m\n")
+      yield if block_given?
+      @output_stream.write("\n\e[0m") 
+    end
+
+    def get_longest_line(text_lines)
+      text_lines.inject(0) do |memo, arg|
+        line_length = arg.to_s.length
+        memo = line_length if line_length > memo
+        memo
+      end
     end
 
     def get_pending_migrations
--- spec/monitor_spec.rb
+++ spec/monitor_spec.rb
@@ -1,33 +1,31 @@
 require 'spec_helper'
 
 describe SchemaMigrationMonitor::Monitor do
-
   before(:each) do
-    MigrationPathService.expects(:execute).returns(MigrationPathService::DEFAULT_MIGRATION_PATH)
+    MigrationPathService.stubs(:execute).returns(MigrationPathService::DEFAULT_MIGRATION_PATH)
   end
 
-  # describe "when ActiveRecord establishes a connection" do
-  #   it "should execute the schema migration monitor" do
-  #     SchemaMigrationMonitor::Monitor.expects(new: nil, execute: nil)
-  #     ActiveRecord::ConnectionAdapters::ConnectionHandler.stubs(:establish_connection_without_monitor)
-      
-  #     ActiveRecord::Base.establish_connection({:adapter => :asdf})
-  #   end
-  # end
-
-  it "should check the pending migrations" do
-    get_migrator_with_pending_migrations([])
-    SchemaMigrationMonitor::Monitor.new.execute
+  describe "when ActiveRecord establishes a connection" do
+    it "should also execute the schema migration monitor" do
+      SchemaMigrationMonitor::Monitor.expects(:new).returns(mock(execute: nil))
+      ActiveRecord::Base.establish_connection({:adapter => 'sqlite3', database: ":memory:"})
+    end
   end
 
   describe "when there are no pending migrations" do
+    let!(:mock_migrator) do
+      mock('migrator').tap do |migrator|
+        migrator.expects(:pending_migrations).returns([])
+      end
+    end
+
     before(:each) do
-      get_migrator_with_pending_migrations([])
+      SchemaMigrationMonitor::Monitor.any_instance.expects(:migrator).returns(mock_migrator)
     end
 
     it "should do nothing" do
-      output_stream = get_output_stream_mock
-      output_stream.should_receive(:write).exactly(0).times
+      output_stream = mock('output_stream')
+      output_stream.expects(:write).never
       SchemaMigrationMonitor::Monitor.new(output_stream).execute
     end
   end
@@ -35,34 +33,22 @@ describe SchemaMigrationMonitor::Monitor do
   describe "when there are pending migrations" do
     let(:migrations) { [mock('migration1', filename:'20120101000000_fake_migration')] }
 
-    before(:each) do
-      get_migrator_with_pending_migrations(migrations)
+    let!(:mock_migrator) do
+      mock('migrator').tap do |migrator|
+        migrator.expects(:pending_migrations).returns(migrations)
+      end
     end
 
-    let(:output_stream) do
-      result = String.new
-      def result.write(arg)
-        self << arg
-      end
-      result
+    before(:each) do
+      SchemaMigrationMonitor::Monitor.any_instance.expects(:migrator).returns(mock_migrator)
     end
         
     it "should print a message to stdout" do
-      SchemaMigrationMonitor::Monitor.new(output_stream).execute
-      (output_stream =~ /The following migration\(s\) need to be run/).should_not == nil
+      read_io, write_io = IO.pipe
+      SchemaMigrationMonitor::Monitor.new(write_io).execute
+      write_io.close_write
+      read_io.read.should match(/The following migration\(s\) need to be run/)
     end
-  end
-
-
-  def get_migrator_with_pending_migrations(pending_migrations)
-    mock_migrator = mock('migrator') 
-    mock_migrator.expects(:pending_migrations).returns(pending_migrations)
-    ActiveRecord::Migrator.expects(:new).returns(mock_migrator)
-    mock_migrator
-  end
-
-  def get_output_stream_mock
-    mock('output_stream')
   end
 
 end
